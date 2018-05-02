04ba0524f22aee7047e8f5321fb290ee3260ff6d
Emma Goldberg <eeg@hosta>
2018-05-02 09:17:02 -0500

    a set of bisse trees and their traits

---
+++ practice_data/T-13983/script.R
@@ -0,0 +1,15 @@
+### A set of bisse trees
+
+library("phycombR")
+
+set.seed(0)
+
+pars <- c(15, 6, 4.5, 4.5, 7.5, 0.75)
+
+phy_all <- trees(pars, type="bisse", n=25, max.taxa=Inf, max.t=1,
+                 include.extinct=FALSE, x0=NA)
+phy_keep <- require_both_states(phy_all, 10)[1:15]
+
+write_trees(phy_keep)
+
+system("mkdir ../A-11463 ; mv s*.csv ../A-11463")
